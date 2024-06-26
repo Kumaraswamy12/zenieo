a53e4da43f0d1256b786f60abd766ba9e778d99f
Kumaraswamy12 <kumaraswamy23082003@gmail.com>
2024-06-26 17:54:55 +0530

    paddle done

--- src/scene/Game.js
+++ src/scene/Game.js
@@ -10,10 +10,18 @@ class Game extends Phaser.Scene
     {
        const ball= this.add.circle(400,250,10,0xffffff,1)
        this.physics.add.existing(ball)
+       ball.body.setBounce(1,1)
 
        ball.body.setCollideWorldBounds(true,1,1)
 
-       ball.body.setVelocity(200,200)
+       ball.body.setVelocity(-200,0)
+
+       const paddleleft = this.add.rectangle(20,250,20,50,0xffffff)
+       this.physics.add.existing(paddleleft,true)       
+
+
+       this.physics.add.collider(paddleleft,ball)
+
 
        
     }
